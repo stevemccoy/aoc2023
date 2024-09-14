23b856c50b8b23a2d80000a881d2fe9ded66a15d
Steve McCoy <stephen.a.mccoy@gmail.com>
2024-09-14 17:52:35 +0100

    Day 14 part 2 done

--- cpp/aoc2023/Day14.cpp
+++ cpp/aoc2023/Day14.cpp
@@ -8,6 +8,10 @@ void Day14::execute()
     part1("input/test14.txt");
     std::cout << "Part 1. Puzzle Input..." << std::endl;
     part1("input/input14.txt");
+    //std::cout << "Part 2. Test Input..." << std::endl;
+    //part2("input/test14.txt");
+    std::cout << "Part 2. Puzzle Input..." << std::endl;
+    part2("input/input14.txt");
 }
 
 void Day14::part1(const char* fileName)
@@ -21,6 +25,11 @@ void Day14::part1(const char* fileName)
 
 void Day14::part2(const char* fileName)
 {
+    auto lines = read_input_file(fileName);
+    process_lines(lines);
+    roll_n_cycles(big_number);
+    int total = calculate_load();
+    std::cout << "Total load is " << total << endl;
 }
 
 void Day14::process_lines(const std::vector<std::string>& lines)
@@ -48,14 +57,102 @@ void Day14::roll_north()
         for (int i = 1; i < numRows; i++) {
             if (get(j, i) == 'O') {
                 // Roll the rock north.
+                bool changed = false;
                 int k = i - 1;
                 while (k >= 0 && get(j, k) == '.') {
                     k--;
+                    changed = true;
                 }
-                set(j, i, '.');
-                set(j, k + 1, 'O');
+                if (changed) {
+                    set(j, i, '.');
+                    set(j, k + 1, 'O');
+                }
             }
         }
+    }
+}
+
+void Day14::roll_south()
+{
+    for (int j = 0; j < numCols; j++) {
+        for (int i = numRows - 2; i >= 0; i--) {
+            if (get(j, i) == 'O') {
+                // Roll the rock south.
+                bool changed = false;
+                int k = i + 1;
+                while (k < numRows && get(j, k) == '.') {
+                    k++;
+                    changed = true;
+                }
+                if (changed) {
+                    set(j, i, '.');
+                    set(j, k - 1, 'O');
+                }
+            }
+        }
+    }
+}
+
+void Day14::roll_west()
+{
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 1; j < numCols; j++) {
+            if (get(j, i) == 'O') {
+                // Roll the rock west.
+                bool changed = false;
+                int k = j - 1;
+                while (k >= 0 && get(k, i) == '.') {
+                    k--;
+                    changed = true;
+                }
+                if (changed) {
+                    set(j, i, '.');
+                    set(k + 1, i, 'O');
+                }
+            }
+        }
+    }
+}
+
+void Day14::roll_east()
+{
+    for (int i = 0; i < numRows; i++) {
+        for (int j = numCols - 2; j >= 0; j--) {
+            if (get(j, i) == 'O') {
+                // Roll the rock east.
+                bool changed = false;
+                int k = j + 1;
+                while (k < numCols && get(k, i) == '.') {
+                    k++;
+                    changed = true;
+                }
+                if (changed) {
+                    set(j, i, '.');
+                    set(k - 1, i, 'O');
+                }
+            }
+        }
+    }
+}
+
+void Day14::roll_one_cycle()
+{
+    roll_north();
+    roll_west();
+    roll_south();
+    roll_east();
+}
+
+void Day14::roll_n_cycles(long n)
+{
+    std::cout << std::endl;
+    int dc = 1000000;
+    while (n--) {
+        if (dc-- == 0) {
+            dc = 1000000;
+        }
+        roll_one_cycle();
+        std::cout << n << "(" << n % 7 << ") -- " << calculate_load() << std::endl;
     }
 }
 
--- cpp/aoc2023/Day14.h
+++ cpp/aoc2023/Day14.h
@@ -19,12 +19,20 @@ private:
 	int numCols;
 	char* grid;
 
+	static const long big_number = 1000000000L;
+
 	char get(int c, int r) { return grid[c + r * numCols]; }
 	void set(int c, int r, char v) { grid[c + r * numCols] = v; }
 
 	void process_lines(const std::vector<std::string>& lines);
 	void roll_north();
+	void roll_south();
+	void roll_west();
+	void roll_east();
+
+	void roll_one_cycle();
+	void roll_n_cycles(long n);
+
 	int calculate_load();
 
 };
-
